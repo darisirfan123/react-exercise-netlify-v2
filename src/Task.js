32a3631145e51f80a30bbd80f4d06c7f447b8ca0
Daris Irfan <20082010001@student.upnjatim.ac.id>
2023-06-17 22:56:39 +0700

    add url

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://magenta-semolina-033b0f.netlify.app/" // TODO: replace this
 const studentName = "Daris Irfan Putrawanto" // TODO: replace this
 const studentId = "FE5498316" // TODO: replace this
 
