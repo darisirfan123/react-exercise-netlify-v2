52a102d67b08e108ffd6715d06f428566a1257cf
Daris Irfan <20082010001@student.upnjatim.ac.id>
2023-06-17 22:54:42 +0700

    add files

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Daris Irfan Putrawanto" // TODO: replace this
+const studentId = "FE5498316" // TODO: replace this
+
+export { url, studentName, studentId }
